f4af59044cb1213d441088cd317d479fb0406bac
samsep <saminsepasi@gmail.com>
2014-12-31 18:57:59 -0800

    Basic requirement done

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,39 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
-  initialize: function(){
+  initialize: function(params){
+
+    this.on('add', this.enqueue, this);
+    this.on('dequeue', this.dequeue, this);
+    this.on('ended', this.playNext, this);
+  },
+
+  playFirst: function(){
+    this.at(0).play();
+  },
+
+  playNext: function() {
+    this.shift();
+    if (this.length >= 1) {
+      this.playFirst();
+    } else {
+      this.trigger('stop');
+    }
+  },
+
+  enqueue: function(song){
+    if( this.length === 1 ){
+      this.playFirst();
+    }
+  },
+
+  dequeue: function(song){
+    if( this.at(0) === song ){
+      this.playNext();
+    } else {
+      this.remove(song);
+    }
   }
+
 
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,10 +2,16 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+    this.render(this);
+    this.on('change', this.render, this);
   },
 
   render: function() {
-    return this.$el;
+    return this.$el.html('<th>Song Queue</th>').append(
+      this.collection.map(function(song){
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
   }
 
 });
